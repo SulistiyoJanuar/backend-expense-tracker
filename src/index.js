64bd4349e4edcff1814d8c12f3174b932fc5b7a2
SulistiyoJanuar <sulistiyojanuarakbar@gmail.com>
2026-02-01 12:03:17 +0700

    feat(core): add error handling middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const app = express();
 const port = 3000;
 
 const expenseRoutes = require('./routes/expense.routes');
+const errorHandler = require('./middlewares/error-handler.middleware');
 
 app.use(express.json());
 
 app.use('/api', expenseRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
